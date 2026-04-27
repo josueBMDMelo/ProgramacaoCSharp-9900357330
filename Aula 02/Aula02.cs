d84275fb450cbae043fe3095a161e267e05bbb4e
josueBMDMelo <josue.bmdmelo@senacsp.edu.br>
2026-04-27 00:17:26 -0300

    Add files via upload

---
+++ Aula 02/Aula02.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+namespace Aula02; 
+
+public class Program 
+{ 
+    public static void Main() 
+    {
+        int numero = 10; // Soma
+        numero = numero + 5;
+        Console.WriteLine(numero);
+        // Forma simplificada
+        numero += 3; Console.WriteLine(numero);
+        
+        // Subtração 
+        numero = 5 - 2;  
+        numero -= 2; 
+        Console.WriteLine(numero);
+        
+        // Multiplicação
+        double resultado = numero * 2;
+        Console.WriteLine(resultado);
+        
+        // Divisão
+        resultado = numero / 2;
+        Console.WriteLine(resultado);
+        
+        // Módulo (resto da divisão)
+        int resto = numero % 2;
+        Console.WriteLine(resto);
+    }
+}
