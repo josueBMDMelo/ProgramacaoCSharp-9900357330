8efe7e56711684d2841016b26fe0479037bb2961
josueBMDMelo <josue.bmdmelo@senacsp.edu.br>
2026-04-29 16:42:47 -0300

    Add files via upload

---
+++ Aula 09/Aula09_2.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Aula09_2;
+
+public class Program
+{
+    public static void Main()
+    {
+        //organizar e evitar repetição
+        //"organizar em blocos"
+        //Andar() -> maiusculo no inicio do nome
+
+        //public: visível fora da classe, posso chama-lo a qualquer momento
+        //static: pertence ao classe, não ao objeto
+        //private: visível apenas dentro da classe
+        //return: quando não existe "void" deve retornar um valor
+        //void: um método sem necessidade de retorno
+        Program.Main();
+    }
+}
